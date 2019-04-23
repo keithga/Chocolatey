c35e89981a6445ead047271a2249d28ab3ad39aa
Keith Garner <keith.garner@hotmail.com>
2019-04-22 18:16:21 -0700

    ADK 1903

---
+++ MDT/ADK.19H1.Chocolatey.ps1
@@ -0,0 +1,32 @@
+[CmdletBinding()]
+param(
+    $Version = '1903'
+)
+
+$ChocoPackagePE = @{
+    Version = '10.0.18362.1'
+    Tags = 'ADK Winpe WAIK'
+    packageName = 'Windows Assessment and Deployment Kit'
+    friendlyUrl = 'https://msdn.microsoft.com/en-us/windows/hardware/dn913721.aspx'
+    Summary = 'The Windows Assessment and Deployment Kit (Windows ADK) is a collection of tools that you can use to customize, assess, and deploy Windows operating systems to new computers. Installs WinPE ONLY!'
+    url = 'https://go.microsoft.com/fwlink/?linkid=2087112'
+}
+
+
+& $PSScriptRoot\..\Common\Create-ChocolateyExePackage.ps1 -Path $PSSCriptRoot\build\$Version @ChocoPackagePE -ID 'windows-adk-winpe' -Features 'winpe' -args "/quiet /norestart /log $env:temp\winPE_adk.log /features +"
+
+$CommonArgs = '/quiet /norestart /log $env:temp\win_adk.log'
+
+$ChocoPackageADK = @{
+    Version = '10.0.18362.1'
+    Tags = 'ADK Winpe WAIK USMT'
+    packageName = 'Windows Assessment and Deployment Kit'
+    friendlyUrl = 'https://msdn.microsoft.com/en-us/windows/hardware/dn913721.aspx'
+    Summary = 'The Windows Assessment and Deployment Kit (Windows ADK) is a collection of tools that you can use to customize, assess, and deploy Windows operating systems to new computers.'
+    url = 'https://go.microsoft.com/fwlink/?linkid=2086042'
+}
+
+& $PSScriptRoot\..\Common\Create-ChocolateyExePackage.ps1 -Path $PSSCriptRoot\build\$Version @ChocoPackageADK -ID 'windows-adk' -Features 'default' -args "$CommonArgs"
+& $PSScriptRoot\..\Common\Create-ChocolateyExePackage.ps1 -Path $PSSCriptRoot\build\$Version @ChocoPackageADK -ID 'windows-adk-all' -Features 'all' -args "$CommonArgs /features +" -Dependency "id=""windows-adk-winpe"" version = ""$($ChocoPackagePE.Version)"""
+& $PSScriptRoot\..\Common\Create-ChocolateyExePackage.ps1 -Path $PSSCriptRoot\build\$Version @ChocoPackageADK -ID 'windows-adk-deploy' -Features 'common deployment' -args "$CommonArgs /features OptionId.DeploymentTools OptionId.ImagingAndConfigurationDesigner OptionId.UserStateMigrationTool"  -Dependency "id=""windows-adk-winpe"" version = ""$($ChocoPackagePE.Version)"""
+
