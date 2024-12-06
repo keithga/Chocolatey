8c33c9708f7963c5289149532c7a6522d04cf52e
keithga <keithga@keithga.com>
2024-12-05 21:42:41 -0700

    add KB4564442 Hotfix

--- MDT/MDT8456.Chocolatey.ps1
+++ MDT/MDT8456.Chocolatey.ps1
@@ -12,4 +12,22 @@ $ChocoPackage = @{
     url64bit = 'https://download.microsoft.com/download/3/3/9/339BE62D-B4B8-4956-B58D-73C4685FC492/MicrosoftDeploymentToolkit_x64.msi'
 }
 
-& $PSScriptRoot\..\Common\Create-ChocolateyMSIPackage.ps1 -Path $PSScriptRoot\Build\MDT8456 @ChocoPackage
+$Path = "$PSScriptRoot\Build\MDT8456"
+
+& $PSScriptRoot\..\Common\Create-ChocolateyMSIPackage.ps1 -Path $Path @ChocoPackage
+
+
+@'
+
+#Install KB4564442 Hotfix
+    $ZipPackage = @{
+        PackageName = 'KB4564442'
+        URL = 'https://download.microsoft.com/download/3/0/6/306AC1B2-59BE-43B8-8C65-E141EF287A5E/KB4564442/MDT_KB4564442.exe' 
+        UnzipLocation = (get-itemproperty 'HKLM:\software\microsoft\Deployment 4').Install_dir + "\Templates\Distribution\Tools"
+        Checksum = 'CD0BC2EC616BA5F8DDD626D02747B19C90EFA1BA159303F2FF41AE693EC10B3F'
+        ChecksumType = 'sha256'
+    }
+
+Install-ChocolateyZipPackage @ZipPackage
+
+'@ | Out-File -FilePath "$Path\$($ChocoPackage.ID)\tools\chocolateyinstall.ps1" -Encoding utf8 -Append
